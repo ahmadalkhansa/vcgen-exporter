920867892b1d6b055e6933b0de4053503a348dda
ahmad <ahmadkhansa95@gmail.com>
2025-01-01 18:49:37 +0100

    preparing for command testing before serve

--- main.go
+++ main.go
@@ -6,19 +6,21 @@ import (
 	"net/http"
 	"flag"
 	"strconv"
+	"strings"
 )
 
 var (
-	cl = temp{pmic: true}
-	vl = volts{sdramc: true, sdrami: true, sdramp: true}
-	ad = adc{}
-	ck = clock{arm: true, gpu: true, uart: true, emmc: true}
-	th = throttle{}
+	cl = temp{enabled: true, pmic: true}
+	vl = volts{enabled: true, sdramc: true, sdrami: true, sdramp: true}
+	ad = adc{enabled: true}
+	ck = clock{enabled: true, arm: true, gpu: true, uart: true, emmc: true}
+	th = throttle{enabled: true}
 )
 
 var port int
 
 func init() {
+	log.Println("vcgen-exporter initializing...")
 	const (
 		portDefault = 8080
 		portUsage = "Exporter's Listening port"
@@ -28,14 +30,29 @@ func init() {
 
 func main() {
 	flag.Parse()
-	sm := func(w http.ResponseWriter, _ *http.Request) {
+	sm := func(w http.ResponseWriter, r *http.Request) {
+		var resp strings.Builder
 		var col string
-		col += PromOut(cl)
-		col += PromOut(vl)
-		col += PromOut(ad)
-		col += PromOut(ck)
-		col += PromOut(th)
-		io.WriteString(w, col)
+		var err error
+		var erri error
+		log.Printf("%s %s request to %s", r.RemoteAddr, r.Method, r.URL.RequestURI())
+		col, err = PromOut(cl)
+		_, erri = resp.WriteString(col)
+		col, err = PromOut(vl)
+		_, erri = resp.WriteString(col)
+		col, err = PromOut(ad)
+		_, erri = resp.WriteString(col)
+		col, err = PromOut(ck)
+		_, erri = resp.WriteString(col)
+		col, err = PromOut(th)
+		_, erri = resp.WriteString(col)
+		if err != nil {
+			log.Println(err)
+		}
+		if erri != nil {
+			log.Println(erri)
+		}
+		io.WriteString(w, resp.String())
 	}
 
 	http.HandleFunc("/metrics", sm)
